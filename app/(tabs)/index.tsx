ba7c94da440e07850f2599a89d7290183973fe1e
mikkeskaug <mikkeskaug@hotmail.com>
2025-04-09 21:12:26 +0200

    Changes not staged for commit:

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,7 +8,7 @@ import { ThemedView } from '@/components/ThemedView';
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: '#A1CEDD', dark: '#1D3D48' }}
       headerImage={
         <Image
           source={require('@/assets/images/partial-react-logo.png')}
